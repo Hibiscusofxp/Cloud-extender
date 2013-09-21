4e9257b7f71159e83af51586bfa756b37ce5137d
amitshah326 <amitshah@umich.edu>
2013-09-21 05:19:24 -0400

    -gui centered and has a fixed size

--- CEGUI/gui.py
+++ CEGUI/gui.py
@@ -1,45 +1,8 @@
 import wx
 import os
 
-class MainWindow(wx.Frame):
-    def __init__(self, parent, title):
-        self.dirname=''
-
-        # A "-1" in the size parameter instructs wxWidgets to use the default size.
-        # In this case, we select 200px width and the default height.
-        wx.Frame.__init__(self, parent, title=title, size=(500,500))
-        
-        self.CreateStatusBar() # A Statusbar in the bottom of the window
-
-        # Setting up the menu.
-        filemenu= wx.Menu()
-        menuAbout= filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-        menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
-
-        # Creating the menubar.
-        menuBar = wx.MenuBar()
-        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
-        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
-
-        self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
-        button1 = wx.Button(self, id=-1, label="&...", pos=(150, 150), size=(100, 30))
-        self.sizer2.Add(button1, 1, wx.EXPAND)
-
-        # Use some sizers to see layout options
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
-        #self.sizer.Add(self.sizer2, 0, wx.EXPAND)
-
-        #Layout sizers
-        self.SetSizer(self.sizer)
-        self.SetAutoLayout(1)
-        self.sizer.Fit(self)
-        self.Show()
-        
-        # Events.
-        self.Bind(wx.EVT_BUTTON, self.OnOpen, button1)
-        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
-        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
-
+class Example(wx.Frame):
+  
     def OnAbout(self,e):
         # Create a message dialog box
         dlg = wx.MessageDialog(self, " A sample editor \n in wxPython", "About Sample Editor", wx.OK)
@@ -59,7 +22,44 @@ class MainWindow(wx.Frame):
             self.control.SetValue(f.read())
             f.close()
         dlg.Destroy()
+  
+    def __init__(self, parent, title):
+        super(Example, self).__init__(parent, title=title, 
+            size=(500, 400))
+        self.CreateStatusBar() # A Statusbar in the bottom of the window
+        self.dirname=''    
+        self.Centre()
+        
+        # Setting up the menu.
+        filemenu= wx.Menu()
+        menuAbout= filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
 
-app = wx.App(False)
-frame = MainWindow(None, "Cloud Extender")
-app.MainLoop()
+        # Creating the menubar.
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+
+        self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        button1 = wx.Button(self, id=-1, label="&...", pos=(150, 150), size=(100, 30))
+        self.sizer2.Add(button1, 1, wx.EXPAND)
+
+        # Use some sizers to see layout options
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.sizer2, 0, wx.EXPAND)
+
+        #Layout sizers
+        self.SetSizer(self.sizer)
+        self.SetAutoLayout(1)
+        self.Show()
+        
+        # Events.
+        self.Bind(wx.EVT_BUTTON, self.OnOpen, button1)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+
+if __name__ == '__main__':
+  
+        app = wx.App()
+        Example(None, title='Cloud Extender')
+        app.MainLoop()
